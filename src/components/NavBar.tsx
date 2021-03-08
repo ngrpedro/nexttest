ab40c5f1776b9c4b6dbb9a576d47eb1c02c7ac80
pedro <ngrpedro22@gmail.com>
2021-03-08 14:50:46 -0300

    mais um

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,9 +6,30 @@ export default function NavBar() {
   return (
     <>
       <div className="navbar isPrimary">
-        <div className="navbar-band fontAwesomeIcon"></div>
+        <div className="navbar-band ">
+          <a
+            id="a"
+            role="button"
+            className="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navBarPedro"
+            onClick={() => {
+              let v = document.getElementById("a");
+              if (v.classList.contains("is-active")) {
+                v.classList.remove("is-active");
+              } else {
+                v.classList.add("is-active");
+              }
+            }}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
 
-        <div className="navbar-menu">
+        <div className="navbar-menu is-active" id="navBarPedro">
           <span className="icon fas fa-2x fontAwesomeIcon">
             <FontAwesomeIcon icon={faHotel} />
           </span>
